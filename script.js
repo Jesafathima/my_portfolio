028a443999f3ccc7c8ecae792c6cc82432a0930a
Fathima Jesa <jazarf24@gmail.com>
2025-12-13 14:04:44 +0530

    Update email sending with toast notifications
    Refactor email sending logic to use showToast for notifications.

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 
 (function() {
     
-    emailjs.init(i76a5bojJJmJMihhD); 
+    emailjs.init("i76a5bojJJmJMihhD"); 
 })();
 
 window.onload = function() {
     
-    const contactForm = document.getElementById('contact-form');
-    const formStatus = document.getElementById('form-status');
+    const contactForm = document.getElementById('contact-form');   
     const submitButton = document.getElementById('submit-button');
 
     contactForm.addEventListener('submit', function(event) {
         event.preventDefault(); 
-
         submitButton.disabled = true; 
-        formStatus.textContent = 'Sending message...';
-        formStatus.style.color = 'gray';
-
-        const serviceID = service_il8ontc; 
-        const templateID = template_2a0co2q;
+        const serviceID = "service_il8ontc"; 
+        const templateID = "template_2a0co2q";
 
         emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
-                formStatus.textContent = 'Message sent successfully!';
-                formStatus.style.color = 'green';
-                contactForm.reset(); 
-                submitButton.disabled = false; 
-            }, (err) => {
-                formStatus.textContent = 'Failed to send message. Please try again later.';
-                formStatus.style.color = 'red';
-                console.error('EmailJS Error:', err);
-                submitButton.disabled = false; 
+                showToast("Message sent successfully!", "green");
+                contactForm.reset();
+                submitButton.disabled = false;
+            })
+           .catch(() => {
+                showToast("Failed to send message. Try again!", "red");
+                submitButton.disabled = false;
             });
+
     });
 }
+const toast = document.getElementById("toast");
+
+function showToast(message, bgColor) {
+    toast.textContent = message;
+    toast.style.background = bgColor;
+    toast.classList.add("show");
+
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 3000);
+}
+
